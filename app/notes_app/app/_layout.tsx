b30e815df18f7bd9b43f2e534375a240141cf7c3
1Creech12 <aleksandrnac109@gmail.com>
2025-12-11 13:33:22 +0900

    new file:   app/.DS_Store 	deleted:    app/notes_app/app/(tabs)/_layout.tsx 	deleted:    app/notes_app/app/(tabs)/explore.tsx 	deleted:    app/notes_app/app/(tabs)/index.tsx 	modified:   app/notes_app/app/_layout.tsx 	new file:   app/notes_app/app/index.tsx 	deleted:    app/notes_app/app/modal.tsx 	new file:   app/notes_app/app/note/[id].tsx 	new file:   app/notes_app/note.ts

--- app/notes_app/app/_layout.tsx
+++ app/notes_app/app/_layout.tsx
@@ -5,18 +5,14 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="index" options={{ title: "Заметки" }} />
+        <Stack.Screen name="note/[id]" options={{ title: "Заметка" }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
