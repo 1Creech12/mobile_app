1cb61603dd3e335a67408623abf8f36f9c0f653a
1Creech12 <aleksandrnac109@gmail.com>
2025-12-24 11:05:12 +0900

    modified:   app/weather_2-app/app/index.tsx

--- app/weather_2-app/app/index.tsx
+++ app/weather_2-app/app/index.tsx
@@ -1,13 +1,27 @@
-import { GesturehandlerRootViev } from "react-native-gesture-handler";
-
+import { GesturehandlerRootViev } from 'react-native-gesture-handler';
+import { SafeAreaProvider, SafeAreaViev} from 'react-native-gesture-handler';
+import { ScrollView, Viev } from 'react-native'
+import InputSearch from '@/components/ui/Input/Input'
 export default function Index() {
     return (
         <GestureHandlerRootViev>
             <SafeAreaProvider>
                 <SafeAreaViev>
 
-                    
+                    <View>
+                        <InputSearch
+                            value={''}
+                            onChangeText={text}
+                        />
 
+                        <Button onPress={test}>
+                            <Ionicons name="settings-outline" size={24} color="black" />
+                        </Button>      
+                    </View>
+
+                    <ScrollViev>
+                        <Hero />
+                    </ScrollViev>
                 </SafeAreaViev>
             </SafeAreaProvider>
         </GestureHandlerRootViev>
