39c84571845fd5d7f2c496389c8f4f77eadecd73
1Creech12 <aleksandrnac109@gmail.com>
2026-01-28 20:56:21 +0900

    new file:   app_new/MyFirstKotlin/Greeting.jar 	new file:   app_new/MyFirstKotlin/Greeting.kt 	new file:   app_new/MyFirstKotlin/Variable.jar 	new file:   app_new/MyFirstKotlin/Variable.kt 	deleted:    app_new/test1.kt 	new file:   "\320\242\320\265\321\201\321\202.txt"

---
+++ app_new/MyFirstKotlin/Variable.kt
@@ -0,0 +1,32 @@
+fun main() {
+    task2()
+    task3()
+    task4()
+}
+
+fun task2() {
+    val name: String = "Александр"
+    var age: Int = 17
+    println("Меня зовут $name, мне $age лет.")
+    age += 1
+    println("Через год мне будет $age лет.")
+}
+
+fun task3() {
+    val hexNumber: Int = 0xFF
+    val binaryNumber: Int = 0b1010
+    val longNumber: Long = 100_000L
+    val floatNumber: Float = 3.14F
+
+    println("Hex: $hexNumber, Binary: $binaryNumber, Long: $longNumber, Float: $floatNumber")
+}
+
+fun task4() {
+    val isSunny = true
+    val isRainy = false
+
+    # Первый результат - False, потому что оба условия должны быть истинными
+    println("isSunny && isRainy = ${isSunny && isRainy}")
+    println("isSunny || isRainy = ${isSunny || isRainy}")
+    println("!isRainy = ${!isRainy}")
+}
